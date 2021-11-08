92b8f57082ad33c2acf2a212a32af3208cadb623
ad_victoriam_101 <keeper24@gmail.com>
2021-11-08 10:37:08 -0800

    first SQL creation from SQLAlchemy

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
-#initialized our app
-app = Flask(__name__)
-#listen to a route
-#DECORATOR FUNCTION
-@app.route('/')
-def root():
-    #what happens when someone goes to the homepage.
-    return 'HELLO WORLD'
+from flask import Flask ,render_template
 
-@app.route('/test')
-def test():
-    return "TEST WORLD"
-    
+def create_app():
+    #initialized our app
+    app = Flask(__name__)
+    #listen to a route
+    #DECORATOR FUNCTION
+    @app.route('/')
+    def root():
+        #what happens when someone goes to the homepage.
+        return render_template('base.html', title='Home')
+
+    @app.route('/test')
+    def test():
+        return 'TEST'
+
+    return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,1 +1,25 @@
-        
+from flask_sqlalchemy import SQLAlchemy
+
+#create db object
+
+DB = SQLAlchemy 
+
+#Make a user table by creating user class
+class User(DB.Model):
+    #id column
+    id = DB.Column(DB.BigInteger,primary_key = True)
+    #username column
+    user_name = DB.Column(DB.String,nullable = False)
+
+#Make a tweet table by creating a tweet class
+class Tweet(DB.Model):
+    #create a tweet ID column
+    id = DB.Column(DB.BigInteger,primary_key = True)
+    #create a text column Unicode allows for Emoji's and links/strings
+    text = DB.Column(DB.Unicode(300))
+    #create a relationship between tweet and user a ONE TO MANY (one user many tweets)
+
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id',nullable=False))
+
+    #finalize the relationship (TWO WAY)
+    user = DB.relationship('User',backref=DB.backref('tweets',lazy=True))
