f36595ad7944b4ad82133510ba4d6419fda6e94b
ad_victoriam_101 <keeper24@gmail.com>
2021-11-08 19:16:19 -0800

    fixed styling.

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,17 +1,71 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template
+from .models import DB, User, Tweet
+
 
 def create_app():
-    #initialized our app
+    # initializes our app
     app = Flask(__name__)
-    #listen to a route
-    #DECORATOR FUNCTION
+
+    # Database configurations
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite3'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Give our APP access to our database
+    DB.init_app(app)
+
+    # Listen to a "route"
+    # '/' is the home page route
     @app.route('/')
     def root():
-        #what happens when someone goes to the homepage.
-        return render_template('base.html', title='Home')
+        # query the DB for all users
+        users = User.query.all()
+        #what I want to happen when somebody goes to home page
+        return render_template('base.html', title="Home", users=users)
 
-    @app.route('/test')
-    def test():
-        return 'TEST'
+    @app.route('/populate')
+    def populate():
+        ryan = User(id=1, username='Ryan')
+        DB.session.add(ryan)
+        julian = User(id=2, username='Julian')
+        DB.session.add(julian)
+        brogan = User(id=3, username='Brogan')
+        DB.session.add(brogan)
+        derick = User(id=4, username='Derick')
+        DB.session.add(derick)
+        steve = User(id=5, username='Steve')
+        DB.session.add(steve)
+        danielle = User(id=6, username='Danielle')
+        DB.session.add(danielle)
+        tweet1 = Tweet(id=1, text='tweet text', user=ryan)
+        DB.session.add(tweet1)
+        tweet2 = Tweet(id=2, text="julian's tweet", user=julian)
+        DB.session.add(tweet2)
+        tweet3 = Tweet(id= 3, text= 'Who put this rock here?', user= brogan)
+        DB.session.add(tweet3)
+        tweet4 = Tweet(id=1, text='New phone who dis', user=ryan)
+        DB.session.add(tweet1)
+        tweet5 = Tweet(id=2, text="Just drove my car into a lake", user=julian)
+        DB.session.add(tweet2)
+        tweet6 = Tweet(id= 3, text= 'Who has a car in the lake?', user= brogan)
+        # save the database
+        DB.session.commit()
+        return '''Created some users. 
+        <a href='/'>Go to Home</a>
+        <a href='/reset'>Go to reset</a>
+        <a href='/populate'>Go to populate</a>'''
+        
+    @app.route('/reset')
+    def reset():
+        # remove everything from the database
+        DB.drop_all()
+        # Creates the database file initially.
+        DB.create_all()
+        return '''The database has been reset. 
+        <a href='/'>Go to Home</a>
+        <a href='/reset'>Go to reset</a>
+        <a href='/populate'>Go to populate</a>'''
+
+
+
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,25 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
-#create db object
+# Create a DB Object
+DB = SQLAlchemy()
 
-DB = SQLAlchemy 
-
-#Make a user table by creating user class
+# Make a User table by creating a User class
 class User(DB.Model):
-    #id column
-    id = DB.Column(DB.BigInteger,primary_key = True)
-    #username column
-    user_name = DB.Column(DB.String,nullable = False)
+    '''Creates a User Table with SQLAlchemy'''
+    # id column
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    # username column
+    username = DB.Column(DB.String, nullable=False)
 
-#Make a tweet table by creating a tweet class
+
+# Make a Tweet table by creating a Tweet class
 class Tweet(DB.Model):
-    #create a tweet ID column
-    id = DB.Column(DB.BigInteger,primary_key = True)
-    #create a text column Unicode allows for Emoji's and links/strings
-    text = DB.Column(DB.Unicode(300))
-    #create a relationship between tweet and user a ONE TO MANY (one user many tweets)
+    '''Creates a Tweet Table with SQLAlchemy'''
+    # id column
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    # text column
+    text = DB.Column(DB.Unicode(300)) # Unicode allows for both text and links and emojis, etc.
 
-    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id',nullable=False))
+    # Create a relationship between a tweet and a user
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
 
-    #finalize the relationship (TWO WAY)
-    user = DB.relationship('User',backref=DB.backref('tweets',lazy=True))
+    # Finalizing the relationship making sure it goes both ways. 
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
