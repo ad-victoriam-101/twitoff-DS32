951b321fed79c68081f900ab80622059803ed7cc
ad_victoriam_101 <keeper24@gmail.com>
2021-11-10 19:30:50 -0800

    redid file system. coppied code from provided workbook.

--- twitoff/models.py
+++ twitoff/models.py
@@ -10,7 +10,13 @@ class User(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     # username column
     username = DB.Column(DB.String, nullable=False)
-
+    # keeps track of id for the newest tweet said by user
+    newest_tweet_id = DB.Column(DB.BigInteger)
+    # We don't need a tweets attribute because this is 
+    # automatically being added by the backref in the Tweet model.
+    # tweets = DB.column(DB.String)
+    def __repr__(self):
+        return f'<User: {self.username}>'
 
 # Make a Tweet table by creating a Tweet class
 class Tweet(DB.Model):
@@ -19,9 +25,12 @@ class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     # text column
     text = DB.Column(DB.Unicode(300)) # Unicode allows for both text and links and emojis, etc.
-
     # Create a relationship between a tweet and a user
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-
     # Finalizing the relationship making sure it goes both ways. 
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    # be able to include a word embedding on a tweet
+    vect = DB.Column(DB.PickleType, nullable=False)
+
+    def __repr__(self):
+        return f'<Tweet: {self.text}>'
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,41 +1,81 @@
-    """Handles connection to twitter API via tweepy, 
-    Installs required packages to read env files and secure load keys. 
-    """
-    
+"""Handles connection to twitter API via tweepy, 
+Installs required packages to read env files and secure load keys. 
+"""
+
 from os import getenv
 import tweepy
 import spacy
-from .models import DB,Tweet,User
+from .models import DB, Tweet, User
 
-#get API KEYS and assign them to local vars
-key = getenv('TWITTER_API_KEY')
-secret = getenv('TWITER_API_KEY_SECRET')
+# Get API keys from .env
+KEY = getenv('TWITTER_API_KEY')
+SECRET = getenv('TWITTER_API_KEY_SECRET')
 
-#connect to the twitter API 
-TWITTER_AUTH = tweepy.OAuthHandler(key,secret)
-TWITER = tweepy.API(TWITTER_AUTH)
+# Connect to the Twitter API
+TWITTER_AUTH = tweepy.OAuthHandler(KEY, SECRET)
+TWITTER = tweepy.API(TWITTER_AUTH)
 
-def add_or_update_uer(username):
-    """Takes Username and pulls user twitter data from API
+# Load our pretrained SpaCy Word Embeddings model
+nlp = spacy.load('my_model/')
 
-    Args:
-        username ([string]): [unique twitter handle]
+# Turn tweet text into word embeddings
+def vectorize_tweet(tweet_text):
+    return nlp(tweet_text).vector
+
+# function to query the API for a user 
+# and add the user to the DB.
+def add_or_update_user(username):
     """
-    twitter_user = TWITTER.get_user(screen_name=username)
-    #check if there is a User in our db with this id,
-    #if not create user and generate with this id
-    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id,username=username)
+    Gets twitter user and tweets from twitter DB
+    Gets user by "username" parameter.
+    """
+    try:
+        # gets back twitter user object
+        twitter_user = TWITTER.get_user(screen_name=username)
+        # Either updates or adds user to our DB
+        db_user = (User.query.get(twitter_user.id)) or User(
+            id=twitter_user.id, username=username)
+        DB.session.add(db_user)  # Add user if don't exist
+
+        # Grabbing tweets from "twitter_user"
+        tweets = twitter_user.timeline(
+            count=200,
+            exclude_replies=True,
+            include_rts=False,
+            tweet_mode="extended",
+            since_id=db_user.newest_tweet_id
+        )
+
+        # check to see if the newest tweet in the DB is equal to the newest tweet from the Twitter API, if they're not equal then that means that the user has posted new tweets that we should add to our DB. 
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+
+        # tweets is a list of tweet objects
+        for tweet in tweets:
+            # type(tweet) == object
+            # Turn each tweet into a word embedding. (vectorization)
+            tweet_vector = vectorize_tweet(tweet.full_text)
+            db_tweet = Tweet(
+                id=tweet.id,
+                text=tweet.full_text,
+                vect=tweet_vector
+            )
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+
+    except Exception as e:
+        print("Error processing {}: {}".format(username, e))
+        raise e
+
+    else:
+        DB.session.commit()
+
+
+def get_all_usernames():
+    '''get the usernames of all users that are already in the database'''
+    usernames = []
+    Users = User.query.all()
+    for user in Users:
+        usernames.append(user.username)
     
-    #add user the DB
-    DB.session.add(db_user)
-    
-    tweets = twitter_user.timeline(count= 200, exclude_replies= True, include_rts= False, tweet_mode= 'extended')
-    
-    #add each tweet for a user to the DB.
-    
-    for tweet in tweets:
-        db_tweet= Tweet(id= tweet.id, text= tweet.full_text[:300])
-        db_user.tweets.append(db_tweet)
-        DB.session.add(db_tweet)
-        
-    DB.session.commit()
+    return usernames
